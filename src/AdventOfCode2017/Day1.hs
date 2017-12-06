1abfc25297c67476b46ef8b2af40bf8d5f8da55a
Jamal Safieddien <mister.recursive@gmail.com>
2017-12-06 19:21:33 +0100

    Add bonus chalange solver for day 1

--- src/AdventOfCode2017/Day1.hs
+++ src/AdventOfCode2017/Day1.hs
@@ -2,19 +2,17 @@ module AdventOfCode2017.Day1 where
 
 import qualified Data.Digits as D
 
-readCaptcha :: Integer -> Integer
-readCaptcha input = let captcha = D.digits 10 input in
-    snd . foldl f (last captcha,0) $ captcha
-    where
-        f (prev,summ) curr =
-            if prev == curr then
-                (curr,summ+curr)
-            else
-                (curr,summ)
+readCaptcha ::Integral a =>  ([a] -> [(a,a)]) -> a -> a
+readCaptcha f = sum . map fst . filter (\ x -> fst x == snd x) . f . D.digits 10
+    
+getNext :: [a] -> [(a,a)]
+getNext xs = zip xs (tail xs) ++ [(last xs, head xs)]
 
+getHalfwayItem :: [a] -> [(a,a)]
+getHalfwayItem xs = map f [0 .. l - 1]
+    where l = length xs          
+          f p = (xs !! p, xs !! mod (p + div l 2) l)
 
-getHalfwayItem :: [a] -> Int -> (a,a)
-getHalfwayItem list pos = undefined
 
 solutionDay1 :: IO Integer
 solutionDay1 = undefined
