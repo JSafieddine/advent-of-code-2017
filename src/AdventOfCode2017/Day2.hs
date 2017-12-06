98fe64341ebf0e22a1897b3063d76f55bcb13e53
Jamal Safieddien <mister.recursive@gmail.com>
2017-12-06 19:22:17 +0100

    Start solving day 2

--- src/AdventOfCode2017/Day2.hs
+++ src/AdventOfCode2017/Day2.hs
@@ -1,2 +1,10 @@
 module AdventOfCode2017.Day2 where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+maxDiff :: (Ord a, Num a) =>  [a] -> a
+maxDiff xs = maximum xs - minimum xs
+
+checksum :: (Ord a, Num a) => [[a]] -> a
+checksum = sum . map maxDiff
