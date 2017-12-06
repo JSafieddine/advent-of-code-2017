ff347c1710d9f80070929f6cb9db9d1c40c3ebe6
Jamal Safieddine <jamal.safieddine@dezem.de>
2017-12-06 09:02:32 +0100

    main function for day1 works

--- src/AdventOfCode2017/Day1.hs
+++ src/AdventOfCode2017/Day1.hs
@@ -3,10 +3,17 @@ module AdventOfCode2017.Day1 where
 import qualified Data.Digits as D
 
 readCaptcha :: Integer -> Integer
-readCaptcha captcha = foldl f (0,0) captcha
-    where f (prev,summ) curr = if prev == curr then
-            (curr,summ+curr)
-                                else
-            (curr,summ)
+readCaptcha input = let captcha = D.digits 10 input in
+    snd . foldl f (last captcha,0) $ captcha
+    where
+        f (prev,summ) curr =
+            if prev == curr then
+                (curr,summ+curr)
+            else
+                (curr,summ)
 
+solutionDay1 :: IO Integer
+solutionDay1 = do
+    input <- readFile "Input/Day1.input" :: Integer
+    return . readCaptcha $ input
 
