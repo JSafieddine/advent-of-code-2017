b89c868b09d8d0093e3f807b50810d8b3546e61b
Jamal Safieddine <jamal.safieddine@dezem.de>
2017-12-07 07:44:45 +0100

    Add solution for day 2 and bonus

--- src/AdventOfCode2017/Day2.hs
+++ src/AdventOfCode2017/Day2.hs
@@ -3,8 +3,16 @@ module AdventOfCode2017.Day2 where
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
+
+readMatrix :: Read a => T.Text -> [[a]]
+readMatrix = map (map (read . T.unpack ) . T.split (== '\t')) . T.lines
+
 maxDiff :: (Ord a, Num a) =>  [a] -> a
 maxDiff xs = maximum xs - minimum xs
 
-checksum :: (Ord a, Num a) => [[a]] -> a
-checksum = sum . map maxDiff
+divable :: Integral a => [a] ->  a
+divable xs = head [div x y | x <- xs , y <- xs, x /= y, mod x y == 0]
+
+
+checksum :: (Ord a, Num a) => ([a] -> a) -> [[a]] -> a
+checksum f = sum . map f
